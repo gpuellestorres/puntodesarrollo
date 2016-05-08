12f5aecdd2d731fc64b1ad111ee1e99ced7f0351
gpuellestorres <gpuellestorres@gmail.com>
2016-05-07 23:12:32 -0300

    sendgrid azure

--- contact_me.php
+++ contact_me.php
@@ -49,14 +49,51 @@ $headers = $email_address;
 
 mail($to,$email_subject,$email_body,$headers);//*/
 
-$sendgrid = new SendGrid('username', 'password');
+/*$sendgrid = new SendGrid('username', 'password');
 $mail = new SendGridMail();
 $mail->addTo($to)->
        setFrom($email_address)->
        setSubject($tema)->
        setText($message);
-$sendgrid->smtp->send($mail);
+       //->
+       //setHtml('<strong>Hello World!</strong>');
+$sendgrid->smtp->send($mail);//*/
 
+$url = 'https://api.sendgrid.com/';
+ $user = 'azure_9139b8d19d8d8919ac87387d2b808886@azure.com';
+ $pass = '39OkI9h8mNIt1Gm'; 
+
+ $params = array(
+      'api_user' => $user,
+      'api_key' => $pass,
+      'to' => $to,
+      'subject' => $tema,
+      'html' => $mensaje,
+      'text' => $mensaje,
+      'from' => $email_address,
+   );
+
+ $request = $url.'api/mail.send.json';
+
+ // Generate curl request
+ $session = curl_init($request);
+
+ // Tell curl to use HTTP POST
+ curl_setopt ($session, CURLOPT_POST, true);
+
+ // Tell curl that this is the body of the POST
+ curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+
+ // Tell curl not to return headers, but do return the response
+ curl_setopt($session, CURLOPT_HEADER, false);
+ curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+ // obtain response
+ $response = curl_exec($session);
+ curl_close($session);
+
+ // print everything out
+ //print_r($response);
 
 echo "Su mensaje fue enviado con éxito. Muchas gracias, pronto le llegará una respuesta a su correo electrónico.";
 
