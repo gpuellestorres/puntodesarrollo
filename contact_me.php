ed8fcf86ee73feab080c729f9db4cedfa9f0a0a3
gpuellestorres <gpuellestorres@gmail.com>
2016-05-07 22:56:14 -0300

    PruebaCorreo

--- contact_me.php
+++ contact_me.php
@@ -1,28 +1,64 @@
-<?php
-// check if fields passed are empty
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['message'])		||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-	echo "El mensaje no pudo ser enviado, debe llenar todos los campos solicitados.";
-	return false;
-   }
-	
-$name = $_POST['name'];
-$email_address = $_POST['email'];
-$message = $_POST['message'];
-$tema = "desde la página puntodesarrollo.com";
-	
-// create email body and send it	
-//$to = 'gpuellestorres@gmail.com';
-$to = 'puntodesarrollo@gmail.com';
-$email_subject = $tema;
-$email_body = "Ha recibido un nuevo mensaje desde la página web puntodesarrollo.com\n\n".
-				  " Detalles:\n \nNombre: ".$name ."\n ".
-				  "Correo electrónico: ".$email_address."\n\n Mensaje: \n ".$message;
-$headers = $email_address;
-mail($to,$email_subject,$email_body,$headers);
-echo "Su mensaje fue enviado con éxito. Muchas gracias, pronto le llegará una respuesta a su correo electrónico.";
-return true;			
+<?php
+
+// check if fields passed are empty
+
+/*
+if(empty($_POST['name'])  		||
+
+   empty($_POST['email']) 		||
+
+   empty($_POST['message'])		||
+
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+
+   {
+
+	echo "El mensaje no pudo ser enviado, debe llenar todos los campos solicitados.";
+
+	return false;
+
+   }/
+
+	
+
+$name = $_POST['name'];
+
+$email_address = $_POST['email'];
+
+$message = $_POST['message'];
+
+$tema = "desde la página puntodesarrollo.com";
+
+//*/
+
+$name = "Guillermo";
+
+$email_address = "gpuellestorres@gmail.com";
+
+$message = "Prueba";
+
+$tema = "desde la página puntodesarrollo.com";
+
+// create email body and send it	
+
+//$to = 'gpuellestorres@gmail.com';
+
+$to = 'puntodesarrollo@gmail.com';
+
+$email_subject = $tema;
+
+$email_body = "Ha recibido un nuevo mensaje desde la página web puntodesarrollo.com\n\n".
+
+				  " Detalles:\n \nNombre: ".$name ."\n ".
+
+				  "Correo electrónico: ".$email_address."\n\n Mensaje: \n ".$message;
+
+$headers = $email_address;
+
+mail($to,$email_subject,$email_body,$headers);
+
+echo "Su mensaje fue enviado con éxito. Muchas gracias, pronto le llegará una respuesta a su correo electrónico.";
+
+return true;			
+
 ?>
